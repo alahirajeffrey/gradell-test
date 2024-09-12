26839b803f4873d82ef73f72309e0f4be93840a4
alahirajeffrey <alahirajeffrey@gmail.com>
2024-09-12 23:30:06 +0100

    commit

--- services/user/utils.ts
+++
@@ -1,8 +0,0 @@
-import amqp from "amqplib";
-
-export const publishToQueue = async (queue: string, data: any) => {
-  const connection = await amqp.connect(process.env.RABBITMQ_URL);
-  const channel = await connection.createChannel();
-  await channel.assertQueue(queue, { durable: false });
-  channel.sendToQueue(queue, Buffer.from(JSON.stringify(data)));
-};
