298f0398af6639bdf8f75a221c26f7ee5bcef9de
alahirajeffrey <alahirajeffrey@gmail.com>
2024-09-13 22:26:32 +0100

    order model

---
+++ services/order/src/models.ts
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const OrderSchema = new mongoose.Schema(
+  {
+    productId: { type: String, required: true },
+    deliveryAddress: { type: String, required: true },
+    totalCost: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", OrderSchema);
