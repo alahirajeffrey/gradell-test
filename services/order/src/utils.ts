8eef3f836884cfc6e942274f7fcbaaf5a3495b98
alahirajeffrey <alahirajeffrey@gmail.com>
2024-09-13 22:25:21 +0100

    order utils

---
+++ services/order/src/utils.ts
@@ -0,0 +1,27 @@
+import { Order } from "./models";
+import { OrderType } from "./types";
+
+export const createOrder = async (order: OrderType) => {
+  try {
+    return await Order.create({
+      totalCost: order.totalCost,
+      deliveryAddress: order.deliveryAddress,
+      productId: order.productId,
+    });
+  } catch (error: any) {
+    console.log(error);
+    throw error;
+  }
+};
+
+export const getSingleOrder = async (orderId: string) => {
+  try {
+    const order = await Order.findById(orderId);
+    if (!order) throw new Error("order does not exist");
+
+    return order;
+  } catch (error: any) {
+    console.log(error);
+    throw error;
+  }
+};
