3e3a1634979ee9a901d34c9847765ca616337abd
alahirajeffrey <alahirajeffrey@gmail.com>
2024-09-14 20:53:09 +0100

    code refactor

--- services/user/src/app.ts
+++ services/user/src/app.ts
@@ -1,22 +1,19 @@
 import express, { Request, Response } from "express";
 import * as dotenv from "dotenv";
 import mongoose from "mongoose";
-import router from "./routes";
 import amqplib from "amqplib";
+import { loginUser, registerUser } from "./utils";
 
 // setup dotenv
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/user-db";
 
-// setup express app
-const app = express();
-app.use(express.json());
-app.use("/api", router);
-
 // connect to mongodb
-mongoose.connect(MONGO_URI).then(() => console.log("connected to mongodb"));
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("connected to mongodb"))
+  .catch((err) => console.error("mongodb connection error:", err));
 
 let channel: amqplib.Channel;
 
@@ -29,14 +26,60 @@ const connectRabbitMQ = async () => {
     port: Number(process.env.RABBITMQ_PORT),
   });
   channel = await connection.createChannel();
-  await channel.assertQueue("product_queue");
+  await channel.assertQueue("user_queue");
   await channel.assertQueue("response_queue");
   console.log("rabbitMQ connected");
 };
 
-connectRabbitMQ();
+// start consumer and listen
+async function startConsumer() {
+  await connectRabbitMQ();
 
-// listen for connections
-app.listen(PORT, () => {
-  console.log(`user service running on port ${PORT}`);
-});
+  channel.consume("user_queue", async (msg) => {
+    if (msg !== null) {
+      const { action, data, correlationId } = JSON.parse(
+        msg.content.toString()
+      );
+      console.log("received message:", { action, data, correlationId });
+
+      try {
+        let response;
+
+        // register user
+        if (action === "register") {
+          const user = await registerUser(data);
+          console.log("user registered");
+          response = { success: true, data: user };
+
+          // login user
+        } else if (action === "login") {
+          const token = await loginUser(data);
+          console.log("user logged in");
+          response = { success: true, data: token };
+        }
+        // unknown action
+        else {
+          console.log("unknown action:", action);
+        }
+
+        channel.sendToQueue(
+          "response_queue",
+          Buffer.from(JSON.stringify(response)),
+          { correlationId }
+        );
+
+        channel.ack(msg);
+      } catch (error) {
+        console.error("error processing message:", error);
+      }
+    }
+  });
+}
+
+startConsumer()
+  .catch((error) => {
+    console.log("error starting consumer: ", error);
+  })
+  .then(() => {
+    console.log("user consumer running");
+  });
