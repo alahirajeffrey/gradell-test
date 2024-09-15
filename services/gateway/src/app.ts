031c90e6e0ffad53269134b8c93cbfd74af9c442
alahirajeffrey <alahirajeffrey@gmail.com>
2024-09-15 07:48:05 +0100

    refactored gateway service producer

--- services/gateway/src/app.ts
+++ services/gateway/src/app.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from "express";
 import * as dotenv from "dotenv";
 import router from "./routes";
+import amqplib from "amqplib";
+import { connectRabbitMQ } from "./producer";
 
 // setup dotenv
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
+
+connectRabbitMQ();
 
 // setup express app
 const app = express();
